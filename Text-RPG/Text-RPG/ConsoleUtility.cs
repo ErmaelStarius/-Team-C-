20f9475e14fa7f4b22d2f577c0cfeb17c79cee20
lxogud <167045465+lxogud@users.noreply.github.com>
2024-05-02 09:56:00 +0900

    2024-05-02/Character
    캐릭터 생성부분 추가

---
+++ Text-RPG/Text-RPG/ConsoleUtility.cs
@@ -0,0 +1,74 @@
+﻿namespace Text_RPG
+{
+    internal class ConsoleUtility
+    {
+
+        public static int PromptChoice(int min, int max)
+        {
+            while (true)
+            {
+                //Console.WriteLine("원하시는 번호를 입력해주세요");
+                //Console.Write(">>");
+                if (int.TryParse(Console.ReadLine(), out int choice) && choice >= min && choice <= max)
+                {
+                    return choice;
+                }
+                Console.WriteLine("잘못된 입력입니다. 다시 시도해주세요.");
+            }
+        }
+
+        public static int CharacterChoice(int min, int max)
+        {
+            int YesNo = ConsoleUtility.PromptChoice(1, 2);
+            
+            switch (YesNo)
+            {
+                case 1:
+                    break;
+                case 2:
+                    return YesNo;
+            }
+            return YesNo;
+        }
+
+        internal static void ShowTitle(string title)
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine(title);
+            Console.ResetColor();
+        }
+        public static void PrintTextHighlights(string s1, string s2, string s3 = "")
+        {
+            Console.Write(s1);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write(s2);
+            Console.ResetColor();
+            Console.WriteLine(s3);
+        }
+
+        public static int GetPrintableLength(string str)
+        {
+            int length = 0;
+            foreach (char c in str)
+            {
+                if (char.GetUnicodeCategory(c) == System.Globalization.UnicodeCategory.OtherLetter)
+                {
+                    length += 2; // 한글과 같은 넓은 문자에 대해 길이를 2로 취급
+                }
+                else
+                {
+                    length += 1; // 나머지 문자에 대해 길이를 1로 취급
+                }
+            }
+
+            return length;
+        }
+
+        public static string PadRightForMixedText(string str, int totalLength)
+        {
+            int currentLength = GetPrintableLength(str);
+            int padding = totalLength - currentLength;
+            return str.PadRight(str.Length + padding);
+        }
+    }
+}
---
+++ Text-RPG/Text-RPG/Player.cs
@@ -0,0 +1,38 @@
+﻿namespace Text_RPG
+{
+    internal class Player
+    {
+        //생성자이후에 Set하지 않겠다! => 읽기 전용이라는 말
+        public string Name { get; } //캐릭터명
+        public string Job { get; } //직업
+        public int Level { get; } //레벨
+        public int Exp { get; } //경험치
+        public int Atk { get; } //공격력
+        public int Def { get; } //방어력
+        public int Hp { get; } //체력
+        public int Mp { get; } //마나
+        public int Gold { get; set; } //소지금
+
+        //생성자의 용도 = 기본세팅
+        public Player(string name, string job, int level, int exp,int attack, int deffence, int hp, int mp, int gold)
+        {
+            Name = name;
+            Job = job;
+            Level = level;
+            Exp = exp;
+            Atk = attack;
+            Def = deffence;
+            Hp = hp;
+            Mp = mp;
+            Gold = gold;
+        }
+        
+        private Player player;
+
+        //플레이어 초기설정
+        internal static void InitializeGame(string playerName, string playerjob, int Atk, int Def, int Hp, int Mp)
+        {
+           Player player = new Player(playerName, playerjob, 1, 0, Atk, Def, Hp, Mp, 1500);
+        }
+    }
+}
