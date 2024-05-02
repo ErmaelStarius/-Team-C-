d9619b7ad3da51d7038c02268789ae6ae778fbc8
ErmaelStarius <ermael35@daum.net>
2024-05-02 12:05:14 +0900

    2024-05-02/Character/Image
    병합파일 조정 / 캐릭터 생성
                       / 배틀이미지 수정

--- Text-RPG/Text-RPG/obj/Debug/net8.0/Text-RPG.AssemblyInfo.cs
+++ Text-RPG/Text-RPG/obj/Debug/net8.0/Text-RPG.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Text-RPG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be27fffc31d935368998ac8ebaf2bbe81712ead1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9adb5ea4620067aebff864a41c31a0a6a1295dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Text-RPG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Text-RPG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
