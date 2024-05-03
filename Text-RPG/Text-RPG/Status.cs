a3d7b8c4e66af34aa21e1f7211e2e9cc98de821d
ErmaelStarius <ermael35@daum.net>
2024-05-03 20:59:31 +0900

    2024-05-03/Status
    스테이터스

---
+++ Text-RPG/Text-RPG/Status.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection.Emit;
+
+namespace Text_RPG
+{
+    public class Status
+    {
+        CreateCharacter name = new CreateCharacter();
+        
+        //상태창
+        public void StatusMenu(Player player)
+        {
+
+            Console.Clear();
+
+            ConsoleUtility.ShowTitle("상태보기");
+            Console.WriteLine("캐릭터의 정보가 표기됩니다.");
+
+            ConsoleUtility.PrintTextHighlights($"Lv. ", player.Level.ToString("00"));
+            Console.WriteLine("");
+            Console.WriteLine($"{player.Name} ({player.Job})");
+            
+            ConsoleUtility.PrintTextHighlights("공격력 :", (player.Atk).ToString());
+            ConsoleUtility.PrintTextHighlights("방어력 :", (player.Def).ToString());
+            ConsoleUtility.PrintTextHighlights("체  력 :", (player.Hp).ToString());
+            ConsoleUtility.PrintTextHighlights("마  나 :", (player.Mp).ToString());
+            ConsoleUtility.PrintTextHighlights("Gold :", player.Gold.ToString());
+            Console.WriteLine("");
+
+            Console.WriteLine("0. 뒤로가기");
+            Console.WriteLine("");
+
+            switch (ConsoleUtility.PromptChoice(0, 0))
+            {
+                case 0:
+                    new Main().MainMenu(player);
+                    break;
+            }
+        }
+    }
+} 
