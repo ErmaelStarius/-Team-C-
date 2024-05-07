7ee065a8be677f81002c35801d49f726216bfe71
birdham <139436177+birdham@users.noreply.github.com>
2024-05-07 09:46:15 +0900

    2024_05_07

--- Text-RPG/Text-RPG/obj/Debug/net8.0/Text-RPG.AssemblyInfo.cs
+++ Text-RPG/Text-RPG/obj/Debug/net8.0/Text-RPG.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Text-RPG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+360b0dd63576c21145da3e4a11be88b1fda2891e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e106e729e0f4fdedb7af903d7a6bb2187105a16f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Text-RPG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Text-RPG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
